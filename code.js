3a44736ad8be17f392c06edf9ef6b97a8fe21ded
NayirGalela <galela.nayir@gmail.com>
2024-02-05 14:08:49 +0100

    creation de la fonction pour le spin

--- code.js
+++ code.js
@@ -1,4 +1,16 @@
 //Le code pour Javascript pour le jeu du spin
 
+// il s'agit de la fonction qui renvoi les valeurs aleatoires  et qui est appelee a chaque tour de jeu.
 
-// j'essaye de comprendre le fonctionnement de git
+function choixValeur(tableau) {
+  let resultat = [];
+  for (let i = 0; i < tableau.length; i++) {
+    let randomIndex = Math.floor(Math.random() * tableau.length);
+    resultat.push(tableau[randomIndex]);
+  }
+  return resultat;
+}
+
+
+
+
